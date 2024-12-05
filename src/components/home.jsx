c53f1cef4b7503aa3850e4256ab481f1e80f1199
Necromanof <theokennedy1808@gmail.com>
2024-12-05 10:38:13 +0100

    Fix: Bug Root Page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,4 +1,11 @@
 import { useState } from "react"
 import App from "../App"
 
-<h1>Dashboard des composants</h1>
+function home(){
+    return(
+        <>
+        </>
+    )
+}
+
+export default home;
