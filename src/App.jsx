ec1f24218945cc58bae7052a93de0ab872119cfd
Necromanof <theokennedy1808@gmail.com>
2024-12-10 14:20:34 +0100

    Je sais pas ce que j'ai fais

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
   return componentsStorage ? JSON.parse(componentsStorage) : [];
   });
 
+  //LocalStorage
   useEffect(() => {
     localStorage.setItem('components', JSON.stringify(components));
   }, [components]);
